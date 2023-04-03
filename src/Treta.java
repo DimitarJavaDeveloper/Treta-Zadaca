8bf7a95ac7cede6bd6eb159034b4fc143d9b3556
unknown <dimitarbelcovski29@gmail.com>
2023-04-03 10:46:10 +0200

    first commit

---
+++ src/Treta.java
@@ -0,0 +1,16 @@
+
+public class Treta {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int n=1, i=100;
+			while(n++<i--);
+			//На крајот од циклусот, 
+			//n ќе има вредност од 51, а i ќе има вредност од 50.
+			// вредноста на n по  ќе биде 51.
+		}
+
+	}
+
+}
